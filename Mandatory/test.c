a2a6f8b6772018ab1cf5c43d80b796f07d438139
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-02-20 13:36:44 +0100

    fork

--- Mandatory/test.c
+++ Mandatory/test.c
@@ -6,7 +6,7 @@
 /*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 12:02:21 by ssbaytri          #+#    #+#             */
-/*   Updated: 2025/02/20 12:16:42 by ssbaytri         ###   ########.fr       */
+/*   Updated: 2025/02/20 13:32:33 by ssbaytri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,18 @@
 
 int main()
 {
-	char *args[] = {"cat", "test.c", NULL};
-	char *env[] = {NULL};
-	if (execve("/bin/cat", args, env) == -1)
+	pid_t pid;
+	int status;
+	pid = fork();
+	if (pid == 0)
 	{
-		perror("execve");
-		return (1);
+		printf("I am the child process with pid = %d\n", getppid());
+	}
+	else
+	{
+		printf("I am the parent process with pid of child = %d and my pid = %d\n", pid, getpid());
+		wait(&status);
+		printf("I am the parent process after the child process has finished\n");
 	}
 	return (0);
 }
