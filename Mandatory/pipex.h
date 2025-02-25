cc036a74f563ac95c9aad31548858434dcc5b4d5
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-02-25 01:51:47 +0000

    test2 pipe

--- Mandatory/pipex.h
+++ Mandatory/pipex.h
@@ -6,7 +6,7 @@
 /*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 17:46:19 by ssbaytri          #+#    #+#             */
-/*   Updated: 2025/02/16 18:53:08 by ssbaytri         ###   ########.fr       */
+/*   Updated: 2025/02/25 01:48:44 by ssbaytri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <fcntl.h>
-
+#include <sys/wait.h>
+#include <string.h>
 
 # endif
--- Mandatory/test.c
+++ Mandatory/test.c
@@ -6,7 +6,7 @@
 /*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 12:02:21 by ssbaytri          #+#    #+#             */
-/*   Updated: 2025/02/25 01:45:58 by ssbaytri         ###   ########.fr       */
+/*   Updated: 2025/02/25 01:51:00 by ssbaytri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,30 @@
 int main()
 {
     int fds[2];
-    char str[20];
+    pid_t pid;
 
     pipe(fds);
-    write(fds[1], "hello world", 11);
-    read(fds[0], str, 11);
-    printf("%s\n", str);
+    pid = fork();
+    if (pid < 0)
+    {
+        perror("fork");
+        return (1);
+    }
+    else if (pid == 0)
+    {
+        close(fds[0]);
+        write(fds[1], "hello", 5);
+        close(fds[1]);
+        exit(EXIT_SUCCESS);
+    }
+    else
+    {
+        close(fds[1]);
+        char str[20];
+        read(fds[0], str, 5);
+        printf("parent received: %s\n", str);
+        close(fds[0]);
+        wait(NULL);
+    }
     return (0);
 }
