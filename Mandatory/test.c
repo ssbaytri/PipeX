7b7d3dc18d142768a6c7ad51eb6d74ebdd94a3cf
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-02-25 01:47:44 +0000

    pipe test

--- Mandatory/test.c
+++ Mandatory/test.c
@@ -6,33 +6,20 @@
 /*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 12:02:21 by ssbaytri          #+#    #+#             */
-/*   Updated: 2025/02/24 21:01:51 by ssbaytri         ###   ########.fr       */
+/*   Updated: 2025/02/25 01:45:58 by ssbaytri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int main() {
-    pid_t pid;
+int main()
+{
+    int fds[2];
+    char str[20];
 
-    pid = 9;
-    printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");    
-    printf("Hello1, I am main process (before fork). My PID is %d.\n", getpid());
-    // Create a child process using fork()
-    pid = fork();
-    printf("------------------------------------\n");     
-    printf("Hello1, I am main process (after fork). My PID is %d.\n", getpid());
-    if (pid < 0) {
-        // Fork failed
-        fprintf(stderr, "Fork2 failed.\n");
-        return 1;
-    } else if (pid == 0) {
-        // Child process
-        printf("Hello1, I am the child process! My PID is %d, and my child's PID is %d\n", getpid(), pid);
-    } else {
-        // Parent process
-        printf("Hello1, I am the parent process! My PID is %d, and my child's PID is %d.\n", getpid(), pid);
-    }
-
-    return 0;
+    pipe(fds);
+    write(fds[1], "hello world", 11);
+    read(fds[0], str, 11);
+    printf("%s\n", str);
+    return (0);
 }
