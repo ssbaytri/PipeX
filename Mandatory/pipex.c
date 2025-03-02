09a3ebc8e7b0739d21c7dead5848afbb325a8eb1
ssbaytri <azjoker333@gmail.com>
2025-03-02 03:19:36 +0000

    till today :)

--- Mandatory/pipex.c
+++ Mandatory/pipex.c
@@ -6,11 +6,30 @@
 /*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 22:55:29 by ssbaytri          #+#    #+#             */
-/*   Updated: 2025/03/02 02:57:29 by ssbaytri         ###   ########.fr       */
+/*   Updated: 2025/03/02 03:18:58 by ssbaytri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+char	**parce_args(char *cmd)
+{
+	char **args = malloc(10 * sizeof(char *));
+	int i = 0;
+	char *token = strtok(cmd, " ");
+	while (token != NULL)
+	{
+		args[i++] = token;
+		token = strtok(NULL, " ");
+	}
+	args[i] = NULL;
+	return args;
+}
+
+char	*get_cmd_path(char *cmd)
+{
+	// TODO: GET THE PATH FROM THE ENV
+}
 
 int main(int ac, char **av)
 {	
--- Mandatory/pipex.h
+++ Mandatory/pipex.h
@@ -6,7 +6,7 @@
 /*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 17:46:19 by ssbaytri          #+#    #+#             */
-/*   Updated: 2025/03/02 02:57:06 by ssbaytri         ###   ########.fr       */
+/*   Updated: 2025/03/02 03:15:22 by ssbaytri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@
 # include <stdio.h>
 # include <fcntl.h>
 # include <sys/wait.h>
+# include <strings.h>
 
 # endif
