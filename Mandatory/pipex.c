58e4f497e885b6bf4d59f10079dda543f28ddd87
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-03-02 12:07:47 +0000

    simple pipex

--- Mandatory/pipex.c
+++ Mandatory/pipex.c
@@ -6,7 +6,7 @@
 /*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 22:55:29 by ssbaytri          #+#    #+#             */
-/*   Updated: 2025/03/02 03:18:58 by ssbaytri         ###   ########.fr       */
+/*   Updated: 2025/03/02 12:04:39 by ssbaytri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	**parce_args(char *cmd)
 	char **args = malloc(10 * sizeof(char *));
 	int i = 0;
 	char *token = strtok(cmd, " ");
-	while (token != NULL)
+	while (token)
 	{
 		args[i++] = token;
 		token = strtok(NULL, " ");
@@ -28,7 +28,55 @@ char	**parce_args(char *cmd)
 
 char	*get_cmd_path(char *cmd)
 {
-	// TODO: GET THE PATH FROM THE ENV
+	char *path = getenv("PATH");
+	char *paths = ft_strdup(path);
+	char *token = strtok(paths, ":");
+	while (token)
+	{
+		char full_path[1024];
+		snprintf(full_path, 1024, "%s/%s", token, cmd);
+		if (access(full_path, X_OK) == 0)
+		{
+			free(paths);
+			return ft_strdup(full_path);
+		}
+		token = strtok(NULL, ":");
+	}
+	free(paths);
+	return NULL;
+}
+
+void	excute_cmds(char *cmd, int infd, int outfd)
+{
+	int pid = fork();
+	if (pid == 0)
+	{
+		dup2(infd, 0);
+		dup2(outfd, 1);
+
+		char **args = parce_args(cmd);
+		char *cmd_path = get_cmd_path(args[0]);
+		if (cmd_path)
+		{
+			execve(cmd_path, args, NULL);
+			perror("Error executing command");
+			free(cmd_path);
+		}
+		else{
+			ft_printf("Command not found: %s\n", args[0]);
+		}
+		free(args);
+		exit(1);
+	}
+	else if (pid > 0)
+	{
+		wait(NULL);
+	}
+	else
+	{
+		perror("Error forking");
+		exit(1);
+	}
 }
 
 int main(int ac, char **av)
@@ -51,6 +99,20 @@ int main(int ac, char **av)
 		perror("Error opening outfile");
 		return (1);
 	}
+	
+	int pipefd[2];
+	if (pipe(pipefd) == -1)
+	{
+		perror("Error creating pipe");
+		close(infile);
+		close(outfile);
+		return (1);
+	}
+	excute_cmds(av[2], infile, pipefd[1]);
+	close(pipefd[1]);
+
+	excute_cmds(av[3], pipefd[0], outfile);
+	close(pipefd[0]);
 
 	close(infile);
 	close(outfile);
