fa74718f06f58a17f2994c4ccf3a70adb74936e6
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-03-03 08:37:13 +0000

    validate files

--- Mandatory/pipex.c
+++ Mandatory/pipex.c
@@ -6,115 +6,41 @@
 /*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 22:55:29 by ssbaytri          #+#    #+#             */
-/*   Updated: 2025/03/02 12:04:39 by ssbaytri         ###   ########.fr       */
+/*   Updated: 2025/03/03 08:35:13 by ssbaytri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char	**parce_args(char *cmd)
+int	validate_files(t_pipex *pipex, char *argv[])
 {
-	char **args = malloc(10 * sizeof(char *));
-	int i = 0;
-	char *token = strtok(cmd, " ");
-	while (token)
+	pipex->infile_fd = open(argv[1], O_RDONLY);
+	if (pipex->infile_fd == -1)
 	{
-		args[i++] = token;
-		token = strtok(NULL, " ");
+		perror("infile");
+		return (0);
 	}
-	args[i] = NULL;
-	return args;
+	pipex->outfile_fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (pipex->outfile_fd == -1)
+	{
+		perror("outfile");
+		close(pipex->infile_fd);
+		return (0);
+	}
+	return (1);
 }
 
-char	*get_cmd_path(char *cmd)
+int	main(int argc, char *argv[], char *envp[])
 {
-	char *path = getenv("PATH");
-	char *paths = ft_strdup(path);
-	char *token = strtok(paths, ":");
-	while (token)
-	{
-		char full_path[1024];
-		snprintf(full_path, 1024, "%s/%s", token, cmd);
-		if (access(full_path, X_OK) == 0)
-		{
-			free(paths);
-			return ft_strdup(full_path);
-		}
-		token = strtok(NULL, ":");
-	}
-	free(paths);
-	return NULL;
-}
+	t_pipex	pipex;
 
-void	excute_cmds(char *cmd, int infd, int outfd)
-{
-	int pid = fork();
-	if (pid == 0)
+	(void)envp;
+	if (argc == 5)
 	{
-		dup2(infd, 0);
-		dup2(outfd, 1);
-
-		char **args = parce_args(cmd);
-		char *cmd_path = get_cmd_path(args[0]);
-		if (cmd_path)
-		{
-			execve(cmd_path, args, NULL);
-			perror("Error executing command");
-			free(cmd_path);
-		}
-		else{
-			ft_printf("Command not found: %s\n", args[0]);
-		}
-		free(args);
-		exit(1);
-	}
-	else if (pid > 0)
-	{
-		wait(NULL);
+		if (!validate_files(&pipex, argv))
+			return (1);
 	}
 	else
-	{
-		perror("Error forking");
-		exit(1);
-	}
-}
-
-int main(int ac, char **av)
-{	
-	(void)av;
-	if (ac != 5)
-	{
-		ft_printf("Usage: ./pipex file1 cmd1 cmd2 file2\n");
-		return (1);
-	}
-	int infile = open(av[1], O_RDONLY);
-	if (infile == -1)
-	{
-		perror("Error opening infile");
-		return (1);
-	}
-	int outfile = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (outfile == -1)
-	{
-		perror("Error opening outfile");
-		return (1);
-	}
-	
-	int pipefd[2];
-	if (pipe(pipefd) == -1)
-	{
-		perror("Error creating pipe");
-		close(infile);
-		close(outfile);
-		return (1);
-	}
-	excute_cmds(av[2], infile, pipefd[1]);
-	close(pipefd[1]);
-
-	excute_cmds(av[3], pipefd[0], outfile);
-	close(pipefd[0]);
-
-	close(infile);
-	close(outfile);
+		ft_putstr_fd("Usage: ./pipex file1 cmd1 cmd2 file2\n", 2);
 	return (0);
 }
--- Mandatory/pipex.h
+++ Mandatory/pipex.h
@@ -6,7 +6,7 @@
 /*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 17:46:19 by ssbaytri          #+#    #+#             */
-/*   Updated: 2025/03/03 04:25:47 by ssbaytri         ###   ########.fr       */
+/*   Updated: 2025/03/03 07:45:21 by ssbaytri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,15 @@
 # include <fcntl.h>
 # include <sys/wait.h>
 # include <string.h>
+
+typedef struct s_pipex
+{
+    int     infile_fd;
+    int     outfile_fd;
+    char    **cmd1_args;
+    char    **cmd2_args;
+    char    *cmd1_path;
+    char    *cmd2_path;
+} t_pipex;
 
 # endif
